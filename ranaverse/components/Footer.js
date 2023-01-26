b6d9a33e4a6dab0919a085301a64765a29906865
RanaRabees <adeemaamir1722@gmail.com>
2023-01-26 14:50:44 +0500

    ranaverse project
    ranaverse project

---
+++ ranaverse/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Footer() {
+  return (
+<>
+
+    <nav
+    className="navbar navbar-expand-sm navbar-dark bg-dark"
+    aria-label="Third navbar example"
+  >
+    <div className="container-fluid">
+      <a
+        className="navbar-brand copyright"
+        href="https://officialrajdeepsingh.dev"
+      >
+        {" "}
+        <p> @Copyrights by Rajdeep Singh</p>{" "}
+      </a>
+    </div>
+  </nav>
+
+  </>
+  )
+}
+
+export default Footer
---
+++ ranaverse/pages/footer.js
@@ -0,0 +1,11 @@
+import Footer from "../components/footer"
+
+function footer() {
+  return (
+    <>
+      <Footer />
+    </>
+  )
+}
+
+export default footer
